41cdb23e6ddfbe3951dbf01ca329b609e29519ae
Ankur Patel <ankur2194@gmail.com>
2019-04-02 23:35:23 +0530

    suto pull test commit

--- github-pull.php
+++ github-pull.php
@@ -2,4 +2,5 @@
 
 if ($_POST['payload']) {
     shell_exec('cd /var/www/html/ossn/ && git reset --hard HEAD && git pull');
+    // shell_exec('sudo chown -R www-data:www-data /var/www/html/ossn/');
 }
