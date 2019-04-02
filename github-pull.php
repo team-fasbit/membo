50af1ccea62a5390c01022e90d369a75cfc8b3dc
Ankur Patel <ankur2194@gmail.com>
2019-04-02 23:38:47 +0530

    permission code correction on auto pull

--- github-pull.php
+++ github-pull.php
@@ -1,6 +1,5 @@
 <?php
 
 if ($_POST['payload']) {
-    shell_exec('cd /var/www/html/ossn/ && git reset --hard HEAD && git pull');
-    shell_exec('sudo chown -R www-data:www-data /var/www/html/ossn/');
+    shell_exec('cd /var/www/html/ossn/ && git reset --hard HEAD && git pull && chown -R www-data:www-data /var/www/html/ossn/');
 }
