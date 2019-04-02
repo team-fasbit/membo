162efccd38054c6197eced2a38f9e8a1945cf011
Ankur Patel <ankur2194@gmail.com>
2019-04-02 23:53:34 +0530

    changes on auto pull script

--- github-pull.php
+++ github-pull.php
@@ -1,5 +1,8 @@
 <?php
 
 if ($_POST['payload']) {
-    shell_exec('cd /var/www/html/ossn/ && git reset --hard HEAD && git pull && chown -R www-data:www-data /var/www/html/ossn/');
+    shell_exec('cd /var/www/html/ossn/ && git reset --hard HEAD && git pull && chown -R www-data:www-data /var/www/html/ossn');
+    echo 'success';
+} else {
+    echo 'fail';
 }
